e53aba6a29e28bb2d841492ed58cd22b47000f3f
Burtlin <fox82421020@gmail.com>
2020-08-26 14:15:54 +0800

    update vealuation

--- tools/evaluation.py
+++ tools/evaluation.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from pylab import figure
 
-def plt_confusion_matrix(cm, classes=None, title='', figsize=(14, 12), fontsize=35, cmap=plt.cm.Blues):
+def plt_confusion_matrix(cm, title='', classes=None, x_label=None, y_label=None, figsize=(14, 12), fontsize=35, cmap=plt.cm.Blues):
     '''plot confusion matrix
     
     Args:
@@ -15,26 +15,23 @@ def plt_confusion_matrix(cm, classes=None, title='', figsize=(14, 12), fontsize=
     Retruns:
         fig (matplotlib.figure.Figure)
     '''
-    if classes is None:
+    if not (classes or x_label or y_label):
         classes = range(len(cm))
-        
-#     classes = list(class_dict.keys())
-#    labels = list(set(class_dict.values()))
-#    cm = confusion_matrix(y_true, y_pred, labels=labels)
+    if classes is not None:
+        x_label = classes
+        y_label = classes
     cm2 = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
     print("ACC:" + format(sum(cm.diagonal()) / np.sum(cm)*100, '.2f'))
-#     title = "Confusion matrix"
     
-#    f, ax = plt.subplots(figsize=(14, 12))
     fig = figure(figsize=figsize, facecolor='w')
     plt.imshow(cm2, interpolation='nearest', cmap=cmap, vmin=0, vmax=1)
     plt.title(title, fontsize=20)
     plt.colorbar()
     
-    tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, fontsize=20, color='#0b6008')
-    plt.yticks(tick_marks, classes, fontsize=20, color='#0b6008')
+    tick_marks = np.arange(len(x_label))
+    plt.xticks(tick_marks, x_label, fontsize=20, color='#0b6008')
+    plt.yticks(tick_marks, y_label, fontsize=20, color='#0b6008')
 
     thresh = cm.max() / 2. ; fmt = 'd'
     thresh2 = .5 ; fmt2 = '.2f'
@@ -52,7 +49,6 @@ def plt_confusion_matrix(cm, classes=None, title='', figsize=(14, 12), fontsize=
     plt.ylabel('True label', fontsize=20, color='#ca7b62')
     plt.xlabel('Predicted label', fontsize=20, color='#ca7b62')
     plt.text(0, -0.55, "ACC:" + format(sum(cm.diagonal()) / np.sum(cm)*100, '.2f'), horizontalalignment='center')
-#     plt.text(0, -0.55, "ACC:" + format(sum(cm.diagonal()) / np.sum(cm)*100, '.2f'), horizontalalignment='center')
     return fig
 
 
@@ -61,6 +57,6 @@ if __name__ == '__main__':
     y_actu = [2, 0, 2, 2, 0, 1, 1, 2, 2, 0, 1, 2]
     y_pred = [0, 0, 2, 1, 0, 2, 1, 0, 2, 0, 2, 2]
     cm = confusion_matrix(y_actu, y_pred)
-    plt_cm = plt_confusion_matrix(cm, classes=['a', 'b', 'c'], fontsize=20, title='Example')
-    plt.savefig('Example.png')
+    plt_cm = plt_confusion_matrix(cm, classes=['A', 'B', 'C'], fontsize=20, title='Example')
+    # plt.savefig('Example.png')
     plt.show()
